12b1d666ab2d507cb23c5d7bc5888e313b640576
Zhenya <ndzhenya@mail.ru>
2022-12-21 21:39:50 +0300

    add Task 1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. По двум введённым числам проверять является ли первое квадратом второго 
+Console.Write("Введите первое число ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число ");
+int b = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Проверим является ли первое число квадратом второго");
+if (b*b==a){
+    Console.WriteLine("Первое число является квадратом второго.");
+}
+else {
+    Console.WriteLine("Первое число не является квадратом второго.");
+}

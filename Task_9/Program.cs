6b6c06b20c7142d5be298ba8a8c136fda7d9feb6
Zhenya <ndzhenya@mail.ru>
2022-12-21 22:26:23 +0300

    add Task 9

---
+++ Task_9/Program.cs
@@ -0,0 +1,18 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток
+
+Console.Write("Введите первое число: ");
+Double A = Double.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число: ");
+Double S = Double.Parse(Console.ReadLine()!);
+
+
+   if ( A % S == 0 )
+   {
+        Console.Write("Первое число кратно второму");
+   }
+   else
+   {
+    Console.WriteLine(A/S);
+    Console.WriteLine("Число не кратно");
+   }

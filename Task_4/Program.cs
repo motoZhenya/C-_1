ff17a33d8bc1db532e10ccf106d63161d4d20ec5
Zhenya <ndzhenya@mail.ru>
2022-12-21 21:53:56 +0300

    add Task 4

---
+++ Task_4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Выяснить является ли число чётным
+
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+if (a%2==0)
+{
+    Console.WriteLine("четное число ");
+}
+else
+{
+    Console.WriteLine("нечетное число ");
+}

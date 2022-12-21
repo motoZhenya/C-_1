a1c448962d4bf342e7da10a73d523a419ef82b31
Zhenya <ndzhenya@mail.ru>
2022-12-21 21:44:53 +0300

    add Task 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите номер дня недели ");
+int day = int.Parse(Console.ReadLine()!);
+int a = 1;
+int b = 2;
+int c = 3;
+int d = 4;
+int f = 5;
+int g = 6;
+int i = 7;
+if (day == a)
+{Console.WriteLine("Понедельник");}
+if (day == b)
+{Console.WriteLine("Вторник");}
+if (day == c)
+{Console.WriteLine("Среда");}
+if (day == d)
+{Console.WriteLine("Четверг");}
+if (day == f)
+{Console.WriteLine("Пятница");}
+if (day == g)
+{Console.WriteLine("Суббота");}
+if (day == i)
+{Console.WriteLine("Воскресенье");}

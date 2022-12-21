8162a198134babf134c81b3875dc7fe565806654
Zhenya <ndzhenya@mail.ru>
2022-12-21 22:17:43 +0300

    add Task 7

---
+++ Task_7/Program.cs
@@ -0,0 +1,18 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+int numberA = new Random().Next(10, 99);
+Console.WriteLine(numberA);
+
+if (numberA > 10 && numberA < 99)
+{
+if (numberA / 10 > numberA % 10)
+{
+        Console.WriteLine("Первая цифра больше");
+}
+else
+if (numberA / 10 == numberA % 10)
+{
+        Console.WriteLine("Цифры одинаковы");
+}
+else
+Console.WriteLine("Вторая цифра больше");
+}
